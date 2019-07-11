283b086cc3d567aacf448511cd0b28d13a728f49
yuhuixu1993 <yuhuixu1993@126.com>
2019-07-11 22:46:28 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -79,5 +79,5 @@ PC_DARTS_cifar = Genotype(normal=[('sep_conv_3x3', 1), ('skip_connect', 0), ('se
 PC_DARTS_image = Genotype(normal=[('skip_connect', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 0), ('skip_connect', 1), ('sep_conv_3x3', 1), ('sep_conv_3x3', 3), ('sep_conv_3x3', 1), ('dil_conv_5x5', 4)], normal_concat=range(2, 6), reduce=[('sep_conv_3x3', 0), ('skip_connect', 1), ('dil_conv_5x5', 2), ('max_pool_3x3', 1), ('sep_conv_3x3', 2), ('sep_conv_3x3', 1), ('sep_conv_5x5', 0), ('sep_conv_3x3', 3)], reduce_concat=range(2, 6))
 
 
-DARTS = PC_DARTS_cifar
+PCDARTS = PC_DARTS_cifar
 
