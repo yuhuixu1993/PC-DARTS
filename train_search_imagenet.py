79ba19dff2ebf84ad0f04cbb4cf572db8f797059
Yuhui Xu <yuhuixu@sjtu.edu.cn>
2019-12-03 22:37:48 +0800

    Add files via upload

---
+++ train_search_imagenet.py
@@ -0,0 +1,223 @@
+import os
+import sys
+import time
+import glob
+import numpy as np
+import torch
+import utils
+import logging
+import argparse
+import torch.nn as nn
+import torch.utils
+import torch.nn.functional as F
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torch.backends.cudnn as cudnn
+
+from torch.autograd import Variable
+from model_search_imagenet import Network
+from architect import Architect
+
+
+parser = argparse.ArgumentParser("search on imagenet")
+parser.add_argument('--workers', type=int, default=32, help='number of workers to load dataset')
+parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
+parser.add_argument('--learning_rate', type=float, default=0.5, help='init learning rate')
+parser.add_argument('--learning_rate_min', type=float, default=0.0, help='min learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
+parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
+parser.add_argument('--layers', type=int, default=8, help='total number of layers')
+parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
+parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+parser.add_argument('--seed', type=int, default=2, help='random seed')
+parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--unrolled', action='store_true', default=False, help='use one-step unrolled validation loss')
+parser.add_argument('--arch_learning_rate', type=float, default=6e-3, help='learning rate for arch encoding')
+parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
+parser.add_argument('--begin', type=int, default=35, help='Tepoch begins to train arch_param')
+parser.add_argument('--tmp_data_dir', type=str, default='/tmp/cache/', help='temp data dir')
+parser.add_argument('--note', type=str, default='try', help='note for this run')
+
+args = parser.parse_args()
+
+args.save = 'search-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
+
+
+CLASSES = 1000
+
+def main():
+  if not torch.cuda.is_available():
+    logging.info('no gpu device available')
+    sys.exit(1)
+
+  np.random.seed(args.seed)
+  cudnn.benchmark = True
+  torch.manual_seed(args.seed)
+  cudnn.enabled=True
+  torch.cuda.manual_seed(args.seed)
+  logging.info("args = %s", args)
+
+  criterion = nn.CrossEntropyLoss()
+  criterion = criterion.cuda()
+
+  model = Network(args.init_channels, CLASSES, args.layers, criterion)
+  model = torch.nn.DataParallel(model)
+  model = model.cuda()
+  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+
+  optimizer = torch.optim.SGD(
+      model.parameters(),
+      args.learning_rate,
+      momentum=args.momentum,
+      weight_decay=args.weight_decay)
+  optimizer_a = torch.optim.Adam(model.module.arch_parameters(),lr=args.arch_learning_rate,betas=(0.5,0.999),weight_decay=args.arch_weight_decay)
+  #data preparation, we random sample 10% and 2.5% from training set(each class) as train and val, respectively.
+  #Note that the data sampling can not use torch.utils.data.sampler.SubsetRandomSampler as imagenet is too large.
+  data_dir = os.path.join(args.tmp_data_dir, 'imagenet')
+  traindir = os.path.join(data_dir, 'train')
+  validdir = os.path.join(data_dir, 'val')
+  normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+  train_data = dset.ImageFolder(
+      traindir,
+      transforms.Compose([
+          transforms.RandomResizedCrop(224),
+          transforms.RandomHorizontalFlip(),
+          transforms.ToTensor(),
+          normalize,
+      ]))
+  valid_data = dset.ImageFolder(
+      validdir,
+      transforms.Compose([
+          transforms.RandomResizedCrop(224),
+          transforms.RandomHorizontalFlip(),
+          transforms.ToTensor(),
+          normalize,
+      ]))
+
+  train_queue = torch.utils.data.DataLoader(
+      train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=args.workers)
+
+  valid_queue = torch.utils.data.DataLoader(
+      valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=args.workers)
+
+  scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, float(args.epochs), eta_min=args.learning_rate_min)
+
+
+  for epoch in range(args.epochs):
+    scheduler.step()
+    lr = scheduler.get_lr()[0]
+    logging.info('epoch %d lr %e', epoch, lr)
+    if epoch < 5 and args.batch_size > 256:
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = current_lr * (epoch + 1) / 5.0
+            logging.info('Warming-up Epoch: %d, LR: %e', epoch, current_lr * (epoch + 1) / 5.0)    genotype = model.genotype()
+    genotype = model.module.genotype()
+    logging.info('genotype = %s', genotype)
+    arch_param = model.module.arch_parameters()
+    print(F.softmax(arch_param[0], dim=-1))
+    print(F.softmax(arch_param[1], dim=-1))
+
+    # training
+    train_acc, train_obj = train(train_queue, valid_queue, model, optimizer, optimizer_a, criterion, lr,epoch)
+    logging.info('train_acc %f', train_acc)
+
+    # validation
+    if args.epochs-epoch<=1:
+      valid_acc, valid_obj = infer(valid_queue, model, criterion)
+      logging.info('valid_acc %f', valid_acc)
+
+    utils.save(model, os.path.join(args.save, 'weights.pt'))
+
+
+def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr,epoch):
+  objs = utils.AvgrageMeter()
+  top1 = utils.AvgrageMeter()
+  top5 = utils.AvgrageMeter()
+
+  for step, (input, target) in enumerate(train_queue):
+    model.train()
+    n = input.size(0)
+    input = input.cuda(non_blocking=True)
+    target = target.cuda(non_blocking=True)
+
+
+    try:
+      input_search, target_search = next(valid_queue_iter)
+    except:
+      valid_queue_iter = iter(valid_queue)
+      input_search, target_search = next(valid_queue_iter)
+    
+    input_search = input_search.cuda(non_blocking=True)
+    target_search = target_search.cuda(non_blocking=True)
+
+    if epoch>=args.begin:
+      optimizer_a.zero_grad()
+      logits = model(input_search)
+      loss_a = criterion(logits,target_search)
+      loss_a.sum().backward()
+      nn.utils.clip_grad_norm_(model.module.arch_parameters(), args.grad_clip)
+      optimizer_a.step()
+
+
+    optimizer.zero_grad()
+    logits = model(input)
+    loss = criterion(logits, target)
+
+    loss.backward()
+    nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
+    optimizer.step()
+
+    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    objs.update(loss.data.item(), n)
+    top1.update(prec1.data.item(), n)
+    top5.update(prec5.data.item(), n)
+
+    if step % args.report_freq == 0:
+      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+
+  return top1.avg, objs.avg
+
+
+def infer(valid_queue, model, criterion):
+  objs = utils.AvgrageMeter()
+  top1 = utils.AvgrageMeter()
+  top5 = utils.AvgrageMeter()
+  model.eval()
+
+  for step, (input, target) in enumerate(valid_queue):
+    input = input.cuda()
+    target = target.cuda(non_blocking=True)
+    with torch.no_grad():
+      logits = model(input)
+      loss = criterion(logits, target)
+
+    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    n = input.size(0)
+    objs.update(loss.data.item(), n)
+    top1.update(prec1.data.item(), n)
+    top5.update(prec5.data.item(), n)
+
+    if step % args.report_freq == 0:
+      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+
+  return top1.avg, objs.avg
+
+
+if __name__ == '__main__':
+  main() 
+
